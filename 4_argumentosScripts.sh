0381c26dba78942f4ff1a898c5812344fedea206
spaezsuarez <spaezsuarez@gmail.com>
2020-09-22 13:48:21 -0500

    Ejemplo uso de argumentos en el script

---
+++ 4_argumentosScripts.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+# Programa para ejemplificar el paso de argumentos
+
+nombreIngresado=$1
+edad=$2
+
+echo "El nombre ingresado es: $nombreIngresado"
+echo "La edad de la persona es: $edad"
