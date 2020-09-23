59a5756051c43506d2954fbce7c613840f56ce2d
spaezsuarez <spaezsuarez@gmail.com>
2020-09-23 10:30:35 -0500

    Descarga de archivos

---
+++ 10_descargaDeArchivos.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+# Programa para ejemplificar el uso de la descarga de informacion desde internet usando wget
+
+echo "Descargar informacion de internet"
+wget https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.zip
