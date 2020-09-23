10409833b5b1f786bb4b3e44466e4168d65728bb
spaezsuarez <spaezsuarez@gmail.com>
2020-09-23 10:22:19 -0500

    Correcciones en el paso de parametros

--- 8_opciones.sh
+++ 8_opciones.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #Programa para ejemplificar el paso de opciones con o sin parametros
-#Autor: Sergio David Paez Suarez
+#Autor: Sergio David Paez Suarez - spaezsuarez@gmail.com
 
-echo "Programas Opciones"
+echo -e  "\n Programas Opciones"
 echo "Opcion 1 enviada: $1"
 echo -e  "Opcion 2 enviada: $2 \n"
 echo -e "Opciones enviadas: $* \n"
 echo "Recuperar valores"
 while [ -n "$1" ]
-do case "$1" in
+do 
+case "$1" in
 -a) echo "-a opcion utilizada";;
 -b) echo "-b opcion utilziada";;
 -c) echo "-c opcion utilizada";;
@@ -16,3 +17,4 @@ do case "$1" in
 esac
 shift
 done
+echo -e "\n"
