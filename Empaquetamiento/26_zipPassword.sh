a7bca1ac52c286ef0cd298ca48aee623b90eedba
spaezsuarez <spaezsuarez@gmail.com>
2020-09-24 09:15:10 -0500

    Empaquetamiento con clave usando zip

---
+++ Empaquetamiento/26_zipPassword.sh
@@ -0,0 +1,5 @@
+# !bin/bash
+# Programa para ejemplificar el empaquetamiento con clave utilizando zip
+# Autor: Sergio David Paez Suarez - spaezsuarez@gmail.com
+zip -e shellCourse.zip ../Manejo_Archivos/*.sh
+
